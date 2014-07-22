0e76d55b5b5c20e20dacdfba264d8d8fcc1d11fa
conceito <conceitocomunica@gmail.com>
2014-07-22 10:36:34 -0300

    removing log files

--- app/logs/log-2014-03-10.php
+++
@@ -1,7 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
-
-ERROR - 2014-03-10 13:16:15 --> Could not find the language line "profiler_get"
-ERROR - 2014-03-10 13:16:15 --> Could not find the language line "profiler_get"
-ERROR - 2014-03-10 13:16:15 --> Could not find the language line "profiler_post"
-ERROR - 2014-03-10 13:16:15 --> Could not find the language line "profiler_post"
-ERROR - 2014-03-10 13:16:15 --> Could not find the language line "profiler_http_headers"
---
+++ info.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Conceito 11
+ * Date: 21/07/14
+ * Time: 12:39
+ */ 
